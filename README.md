ed4804593b1c78a4e271505b645e32a8ffb0ff55
christianm20358 <christianm20358@gmail.com>
2024-09-26 11:10:55 -0600

    Javadoc and README

---
+++ README.md
@@ -0,0 +1,4 @@
+# HList
+
+A simple implementation of a heterogynous list in Java, allowing items of multiple different types to be stored.
+
--- src/hlist/HList.java
+++ src/hlist/HList.java
@@ -1,23 +1,48 @@
 package hlist;
 
 /**
+ * A list that allows storage of heterogynous elements.
  *
  * @author Christian Miller
+ * @version 1.0 26-Sep-2024
  */
 public class HList {
+    /**
+     * The element at the beginning of the list.
+     */
     private HListNode<Object> listHead;
+    /**
+     * The element at the end of the list.
+     */
     private HListNode<Object> listTail;
+    /**
+     * The number of items contained in the list.
+     */
     private int numberOfItems;
 
+    /**
+     * Determine if the list has any elements stored.
+     *
+     * @return true if the list is empty, otherwise false.
+     */
     public boolean isListEmpty() {
         return this.listHead == null;
     }
 
+    /**
+     *
+     * @return The number of elements in the list.
+     */
     public int getNumberOfItems() {
         return this.numberOfItems;
     }
 
-    public boolean addItem(Object item) {
+    /**
+     * Adds an object to the end of the list.
+     *
+     * @param item The item to be stored at the end of the list.
+     */
+    public void addItem(Object item) {
         HListNode<Object> newNode = new HListNode<>(item);
 
         if (isListEmpty()) {
@@ -29,9 +54,15 @@ public class HList {
         }
 
         this.numberOfItems++;
-        return true;
     }
 
+    /**
+     * Removes the object matching the input item, or null if no matching items
+     * are found.
+     *
+     * @param value The object matching the element being searched for.
+     * @return The object reference matching the input value, or null.
+     */
     public Object removeItem(Object value) {
         HListNode<Object> current = this.listHead;
         HListNode<Object> previous = null;
@@ -56,6 +87,9 @@ public class HList {
         return null;
     }
 
+    /**
+     * Displays all the elements in the list, printing to stdout.
+     */
     public void displayList() {
         System.out.println("Displaying Heterogynous List");
 
